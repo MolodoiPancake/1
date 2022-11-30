10087fa49ff79f752b2f39fbc0f1796888afdfea
Gorik <gosha_qwerty@rambler.ru>
2022-11-30 14:03:16 +0800

    Task6

---
+++ Task 6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+Console.WriteLine ("Это число четное");
+
+else
+
+Console.WriteLine ("Это число нечетное");
